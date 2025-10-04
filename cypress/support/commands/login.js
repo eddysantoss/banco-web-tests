66a144a137652bcf0724d1335d40499153ecc11d
eddyclayton@gmail.com <eddyclayton@gmail.com>
2025-10-04 14:25:40 -0300

    Implementando comandos customizaveis para Login, Transferencias e Commons

---
+++ cypress/support/commands/login.js
@@ -0,0 +1,18 @@
+Cypress.Commands.add('fazerLoginComCredenciaisVálidas', () => {
+    cy.fixture('credenciais').then(credenciais => {
+        cy.get('#username').click().type(credenciais.valida.usuario)
+        cy.get('#senha').click().type(credenciais.valida.senha)
+  
+      })
+        
+      cy.contains('button', 'Entrar').click()
+})
+
+Cypress.Commands.add('fazerLoginComCredenciaisInvalidas', () => {
+    cy.fixture('credenciais').then(credenciais => {
+        cy.get('#username').click().type(credenciais.invalida.usuario)
+        cy.get('#senha').click().type(credenciais.invalida.senha)
+  
+    })  
+    cy.contains('button', 'Entrar').click()
+})
